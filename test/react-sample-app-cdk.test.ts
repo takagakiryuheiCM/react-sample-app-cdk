9347eb371a713d90a5efb6194f1f96388d4b5922
takagakiryuheiCM <takagaki.ryuhei@classmethod.jp>
2024-03-13 11:07:02 +0900

    Initial commit

---
+++ test/react-sample-app-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ReactSampleAppCdk from '../lib/react-sample-app-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/react-sample-app-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ReactSampleAppCdk.ReactSampleAppCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
